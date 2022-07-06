8797640e2c114f36361d82da2d96afbb5e8f8763
phoenix <betafoenix@gmail.com>
2022-07-06 10:35:43 +0300

    initial

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: host ?? '127.0.0.1',
+        port: port ?? 5432,
+        database: database ?? 'portfolio',
+        user: user ?? 'portfolio_admin',
+        password: password ?? 'password',
+        ssl: host === 'localhost' ? false : true
+      }
+    }
+  }
+};
